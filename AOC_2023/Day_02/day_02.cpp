377ace1b19cc7b7fd8b9a18589d73a9ec5e7cee5
Hein Soe <aungheinsoe@abank.com.mm>
2025-03-24 13:32:29 +0630

    - Day 02, Part 01

--- AOC_2023/Day_02/day_02.cpp
+++ AOC_2023/Day_02/day_02.cpp
@@ -3,10 +3,12 @@
 #include <vector>
 #include <string>
 #include <chrono>
+#include <array>
+#include <numeric>
 
-std::vector<std::string> read_file()
+std::vector<std::string> read_file(const std::string &file_name)
 {
-    std::ifstream input_file("input.txt");
+    std::ifstream input_file(file_name);
     if (!input_file.is_open())
     {
         std::cerr << "Error opening file\n";
@@ -23,34 +25,188 @@ std::vector<std::string> read_file()
     return lines;
 }
 
-std::string part_one()
+int part_one()
 {
-    return "0";
+    enum COLOR
+    {
+        RED,
+        GREEN,
+        BLUE,
+        COUNT
+    };
+
+    std::array<int, COUNT> max_color_count = {12, 13, 14};
+
+    std::vector<std::string> lines = read_file("input.txt");
+    if (lines.empty())
+    {
+        std::cerr << "File is empty." << std::endl;
+        return {};
+    }
+
+    std::vector<int> result;
+    int game_id = 0;
+    for (const std::string &line : lines)
+    {
+        ++game_id;
+        bool possiable_game = false;
+        int current_color_count = 0;
+        size_t pos = line.find(":");
+        for (size_t i = pos + 2; i < line.size(); ++i)
+        {
+            char color = line[i];
+            if (color == ' ')
+                continue;
+            if (std::isdigit(color))
+            {
+                if (std::isdigit(line[i + 1]))
+                {
+                    current_color_count += (color - '0') * 10 + (line[i + 1] - '0');
+                    i += 1;
+                    continue;
+                }
+                else
+                {
+                    current_color_count += color - '0';
+                    continue;
+                }
+            }
+
+            COLOR c;
+            switch (color)
+            {
+            case 'r':
+                i += 4;
+                c = RED;
+                break;
+            case 'g':
+                i += 6;
+                c = GREEN;
+                break;
+            case 'b':
+                i += 5;
+                c = BLUE;
+                break;
+            default:
+                break;
+            }
+
+            if (current_color_count > max_color_count[c])
+            {
+                possiable_game = false;
+                break;
+            }
+
+            current_color_count = 0;
+            possiable_game = true;
+        }
+        if (possiable_game)
+            result.push_back(game_id);
+    }
+
+    return std::accumulate(result.begin(), result.end(), 0);
 }
 
-std::string part_two()
+int part_two()
 {
-    return "0";
+    enum COLOR
+    {
+        RED,
+        GREEN,
+        BLUE,
+        COUNT
+    };
+
+    std::array<int, COUNT> max_color_count = {12, 13, 14};
+
+    std::vector<std::string> lines = read_file("input.txt");
+    if (lines.empty())
+    {
+        std::cerr << "File is empty." << std::endl;
+        return {};
+    }
+
+    std::vector<int> result;
+    int game_id = 0;
+    for (const std::string &line : lines)
+    {
+        ++game_id;
+        bool possiable_game = false;
+        int current_color_count = 0;
+        size_t pos = line.find(":");
+        for (size_t i = pos + 2; i < line.size(); ++i)
+        {
+            char color = line[i];
+            if (color == ' ')
+                continue;
+            if (std::isdigit(color))
+            {
+                if (std::isdigit(line[i + 1]))
+                {
+                    current_color_count += (color - '0') * 10 + (line[i + 1] - '0');
+                    i += 1;
+                    continue;
+                }
+                else
+                {
+                    current_color_count += color - '0';
+                    continue;
+                }
+            }
+
+            COLOR c;
+            switch (color)
+            {
+            case 'r':
+                i += 4;
+                c = RED;
+                break;
+            case 'g':
+                i += 6;
+                c = GREEN;
+                break;
+            case 'b':
+                i += 5;
+                c = BLUE;
+                break;
+            default:
+                break;
+            }
+
+            if (current_color_count > max_color_count[c])
+            {
+                possiable_game = false;
+                break;
+            }
+
+            current_color_count = 0;
+            possiable_game = true;
+        }
+        if (possiable_game)
+            result.push_back(game_id);
+    }
+
+    return std::accumulate(result.begin(), result.end(), 0);
 }
 
 int main()
 {
     using std::chrono::duration;
-	using std::chrono::duration_cast;
-	using std::chrono::high_resolution_clock;
-	using std::chrono::milliseconds;
+    using std::chrono::duration_cast;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::milliseconds;
 
-	auto start = high_resolution_clock::now();
+    auto start = high_resolution_clock::now();
 
-	std::string p1 = part_one();
-	std::string p2 = part_two();
+    int p1 = part_one();
+    int p2 = part_two();
 
-	std::cout << "Part One: " << p1 << std::endl;
-	std::cout << "Part Two: " << p2 << std::endl;
+    std::cout << "Part One: " << p1 << std::endl;
+    std::cout << "Part Two: " << p2 << std::endl;
 
-	auto stop = high_resolution_clock::now();
-	auto dura = duration_cast<milliseconds>(stop - start);
-	std::cout << "Execution Time: " << dura.count() << " ms." << std::endl;
+    auto stop = high_resolution_clock::now();
+    auto dura = duration_cast<milliseconds>(stop - start);
+    std::cout << "Execution Time: " << dura.count() << " ms." << std::endl;
 
-	return 0;
+    return 0;
 }
