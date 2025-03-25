62c0762ddffbc000eab923bdfe35438b73e82620
Hein Soe <aungheinsoe@abank.com.mm>
2025-03-25 16:49:52 +0630

    - Day 03, Part 02

--- AOC_2023/Day_03/day_03.cpp
+++ AOC_2023/Day_03/day_03.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <chrono>
 #include <numeric>
+#include <map>
 
 std::vector<std::string> read_file()
 {
@@ -120,7 +121,107 @@ std::string part_one()
 
 std::string part_two()
 {
-    return "0";
+    std::vector<long> result;
+    std::vector<std::string> input = read_file();
+    std::vector<std::vector<char>> grid;
+    for (int i = 0; i < input.size(); i++)
+    {
+        std::vector<char> line;
+        for (int j = 0; j < input[i].size(); j++)
+        {
+            if (input[i][j] == '\n')
+                continue;
+            line.push_back(input[i][j]);
+        }
+        grid.push_back(line);
+    }
+
+    int rows = grid.size();
+    int cols = grid[0].size();
+
+    std::vector<int> row_direction = {-1, 0, 1};
+    std::vector<int> col_direction = {-1, 0, 1};
+
+    std::map<std::vector<int>, std::vector<int>> gears;
+
+    for (int row = 0; row < rows; ++row)
+    {
+        int num_str = 0;
+        for (int col = 0; col < cols;)
+        {
+            if (isdigit(grid[row][col]))
+            {
+                // grab the whole number
+                int start = col;
+                int end = 0;
+                while (col < cols && isdigit(grid[row][col]))
+                {
+                    num_str = num_str * 10 + grid[row][col] - '0';
+                    end = ++col;
+                }
+                std::cout << "Found number: " << num_str << " at (" << row << "," << start << ")-(" << row << "," << col - 1 << ")" << std::endl;
+
+                bool adjacent_symbol = false;
+                std::vector<int> found_gear;
+                // mark for current number position, start as start position and
+                for (int k = start; k < end; ++k)
+                {
+                    // std::cout << "Checking digit at (" << row << "," << k << ")" << std::endl;
+                    // loop trough for previous row/col and next row/col for searching symbol
+                    for (int rd = -1; rd <= 1; ++rd)
+                    {
+                        for (int cd = -1; cd <= 1; ++cd)
+                        {
+                            if (rd == 0 && cd == 0)
+                                continue;
+                            int nr = row + rd;
+                            int nc = k + cd;
+                            // std::cout << "position [" << nr << ", " << nc << "]" << std::endl;
+                            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols)
+                            {
+                                // std::cout << "position [" << nr << ", " << nc << "] => " << grid[nr][nc] << std::endl;
+                                if (grid[nr][nc] == '*')
+                                {
+                                    gears[{nr, nc}];
+                                    found_gear.push_back(nr);
+                                    found_gear.push_back(nc);
+                                    adjacent_symbol = true;
+                                    break;
+                                }
+                            }
+                            if (adjacent_symbol)
+                                break;
+                        }
+                        if (adjacent_symbol)
+                            break;
+                    }
+                    if (adjacent_symbol)
+                        break;
+                }
+
+                if (adjacent_symbol)
+                {
+                    gears[found_gear].push_back(num_str);
+                    found_gear.clear();
+                    // result.push_back(num_str);
+                }
+                num_str = 0;
+            }
+            else
+            {
+                ++col;
+            }
+        }
+    }
+
+    for(const auto& gear: gears)
+    {
+        if(gear.second.size() == 2){
+            result.push_back(gear.second[0] * gear.second[1]);
+        }
+    }
+
+    return std::to_string(std::accumulate(result.begin(), result.end(), 0));
 }
 
 int main()
