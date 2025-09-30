d23011c2aa909bf292e8444099355f9dd98f2f90
Hein Soe <aungheinsoe@abank.com.mm>
2025-09-30 16:25:35 +0630

    - Day 18, Part 02

--- AOC_2023/Day_18/day_18.cpp
+++ AOC_2023/Day_18/day_18.cpp
@@ -111,6 +111,31 @@ std::string part_one()
     return std::to_string(result);
 }
 
+std::map<char, Point> two_directions = {
+    {'0', Point(0, 1)},
+    {'1', Point(1, 0)},
+    {'2', Point(0, -1)},
+    {'3', Point(-1, 0)}
+};
+
+DigPlan parse_line_two(const std::string& line)
+{
+    std::istringstream iss(line);
+    std::string dir_str, color;
+    long long length;
+    iss >> dir_str >> length >> color;
+    std::string haxcode = color.substr(2, 5);
+    char dir_char = color[7];
+
+    if (two_directions.find(dir_char) == two_directions.end()) {
+        throw std::invalid_argument("Invalid direction: " + std::string(1, dir_char));
+    }
+
+    length = std::stoll(haxcode, nullptr, 16); // hex -> decimal
+
+    return DigPlan(two_directions[dir_char], length, color);
+}
+
 std::string part_two()
 {
     std::vector<std::string> lines = read_file();
@@ -119,7 +144,41 @@ std::string part_two()
         std::cerr << "No data found." << std::endl;
         return 0;
     }
-    return "0";
+
+    // prepare to digging the trenches
+    std::vector<DigPlan> plans;
+    for(const auto& line: lines)
+    {
+        plans.push_back(parse_line_two(line));
+    }
+    
+    // start digging the trenches
+    Point current_pos(0, 0);
+    std::vector<Point> trenches = {current_pos};
+    long long boundary_point = 0;
+    for(const auto& plan: plans)
+    {
+        Point motion = plan.cube * plan.length;
+        current_pos = current_pos + motion;
+        trenches.push_back(current_pos);
+        boundary_point += plan.length;
+    }
+    
+    // calculate the area using the shoelace formula
+    long long shoelace_sum = 0;
+    int n = trenches.size();
+    for(int i = 0; i < n; i++)
+    {
+        const Point& a = trenches[i]; // get current point
+        const Point& b = trenches[(i + 1) % n]; // get next point and loop back to the start
+        shoelace_sum += a.row * b.column - a.column * b.row;
+    }
+
+    // apply Pick's Theorem to find the number of interior points
+    long long area = std::llabs(shoelace_sum);
+    long long result = (area + boundary_point) / 2 + 1;
+
+    return std::to_string(result);
 }
 
 int main() {
